efa7e1e44b6a728b5360a63a20f514ef13c2787a
abdelaziz266 <zizoelbeltagy266@gmail.com>
2022-07-17 02:27:52 +0200

    upload files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartComponent } from './component/cart/cart.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ProductDetailsComponent } from './component/product-details/product-details.component';
+import { ProductsComponent } from './component/products/products.component';
+import { SelectComponent } from './component/select/select.component';
+import { SpinnerComponent } from './component/spinner/spinner.component';
+import { ViewProductsComponent } from './component/view-products/view-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    HeaderComponent,
+    ProductDetailsComponent,
+    ProductsComponent,
+    SelectComponent,
+    SpinnerComponent,
+    ViewProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
